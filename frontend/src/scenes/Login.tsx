2a82b53108ace4147e62d064ec7c7436862a713f
Corrie Stoddard <corriestoddard@gmail.com>
2025-05-31 18:21:32 -0700

    Add to login screen

--- frontend/src/scenes/Login.tsx
+++ frontend/src/scenes/Login.tsx
@@ -4,17 +4,22 @@ import "../styles/login.scss";
 export default function Login() {
   return (
     <div className="login-container">
-      <h2>User Authorization Required</h2>
-      <form>
-        <div className="form-group">
-          <label htmlFor="username">Name</label>
-          <input type="text" id="username" name="username" required />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Code</label>
-          <input type="password" id="password" name="password" required />
-        </div>
-      </form>
+      <div className="login-form">
+        <h2>User Authorization Required</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">Name</label>
+            <input type="text" id="username" name="username" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Code</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+        </form>
+      </div>
+      <button type="submit" className="login-button">
+        Proceed
+      </button>
     </div>
   );
 }
