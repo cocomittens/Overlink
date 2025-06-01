4f78d3e36eae4d467398e57f590e930f4569fe6f
Corrie Stoddard <corriestoddard@gmail.com>
2025-05-31 20:24:25 -0700

    add active class to map node

--- frontend/src/components/MapNode.tsx
+++ frontend/src/components/MapNode.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "../styles/map.scss";
 
-type MapNodeProps = { top: number; left: number };
-export default function MapNode({ top, left }: MapNodeProps) {
-  return <div className="map-node" style={{ top, left }} />;
+type MapNodeProps = { top: number; left: number; active?: boolean };
+export default function MapNode({ top, left, active = false }: MapNodeProps) {
+  const className = `map-node${active ? " active" : ""}`;
+  return <div className={className} style={{ top, left }} />;
 }
--- frontend/src/scenes/Map.tsx
+++ frontend/src/scenes/Map.tsx
@@ -3,12 +3,12 @@ import "../styles/map.scss";
 import MapNode from "../components/MapNode";
 
 export default function Map() {
-  // example position values in pixels
-  const posX = 100;
-  const posY = 150;
+  const posX = 500;
+  const posY = 250;
   return (
     <div className="map-container">
-      <MapNode top={posY} left={posX} />
+      <MapNode active={true} top={420} left={300} />
+      <MapNode top={200} left={250} />
     </div>
   );
 }
