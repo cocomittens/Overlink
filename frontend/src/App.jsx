333f18adb6a776abf4e58ab82ddbb488390efe5f
Corrie Stoddard <corriestoddard@gmail.com>
2025-05-31 20:01:07 -0700

    Add map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css"
 import Missions from './scenes/Missions.tsx'
 import Login from './scenes/Login.tsx'
+import Map from './scenes/Map.tsx'
 
 function App() {
 
   return (
-    <Missions />
+    <Map />
   )
 }
 
