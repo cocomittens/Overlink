50e0462a51819fa1d5b946093f7e688e73fb2ebd
Corrie Stoddard <corriestoddard@gmail.com>
2025-05-31 20:49:24 -0700

    Add outlines to map nodes

--- frontend/src/components/MapNode.tsx
+++ frontend/src/components/MapNode.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "../styles/map.scss";
 
-type MapNodeProps = { top: number; left: number; active?: boolean };
-export default function MapNode({ top, left, active = false }: MapNodeProps) {
-  const className = `map-node${active ? " active" : ""}`;
+type MapNodeProps = {
+  top: number;
+  left: number;
+  active?: boolean;
+  admin?: boolean;
+  account?: boolean;
+};
+export default function MapNode({
+  top,
+  left,
+  active = false,
+  admin = false,
+  account = false,
+}: MapNodeProps) {
+  const className = [
+    "map-node",
+    active ? "active" : null,
+    admin ? "admin" : null,
+    account ? "account" : null,
+  ]
+    .filter(Boolean)
+    .join(" ");
   return <div className={className} style={{ top, left }} />;
 }
--- frontend/src/scenes/Map.tsx
+++ frontend/src/scenes/Map.tsx
@@ -7,8 +7,8 @@ export default function Map() {
   const posY = 250;
   return (
     <div className="map-container">
-      <MapNode active={true} top={420} left={300} />
-      <MapNode top={200} left={250} />
+      <MapNode active admin top={420} left={300} />
+      <MapNode account top={200} left={250} />
     </div>
   );
 }
