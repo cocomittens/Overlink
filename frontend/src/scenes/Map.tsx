1c78b2cfa5e758a0d6ee421bd4eac19fe7b5f781
Coco <corriestoddard@gmail.com>
2025-06-18 16:54:31 -0700

    Add map buttons

--- frontend/src/scenes/Map.tsx
+++ frontend/src/scenes/Map.tsx
@@ -36,47 +36,53 @@ export default function Map() {
   };
 
   return (
-    <div className="map-container">
-      <svg
-        className="map-lines"
-        xmlns="http://www.w3.org/2000/svg"
-        width="100%"
-        height="100%"
-      >
-        {chain.map((id, i) => {
-          const nextId = chain[i + 1];
-          if (!nextId) return null;
-          const from = nodes.find((n) => n.id === id)!;
-          const to = nodes.find((n) => n.id === nextId)!;
-          const x1 = from.left + 5;
-          const y1 = from.top + 5;
-          const x2 = to.left + 5;
-          const y2 = to.top + 5;
-          return (
-            <line
-              key={`${id}-${nextId}`}
-              x1={x1}
-              y1={y1}
-              x2={x2}
-              y2={y2}
-              stroke="#fff"
-              strokeWidth={2}
-            />
-          );
-        })}
-      </svg>
-      {nodes.map((node) => (
-        <MapNode
-          key={node.id}
-          top={node.top}
-          left={node.left}
-          name={node.name}
-          active={node.active}
-          admin={node.admin}
-          account={node.account}
-          onClick={() => toggleNode(node.id)}
-        />
-      ))}
+    <div>
+      <div className="map-container">
+        <svg
+          className="map-lines"
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          height="100%"
+        >
+          {chain.map((id, i) => {
+            const nextId = chain[i + 1];
+            if (!nextId) return null;
+            const from = nodes.find((n) => n.id === id)!;
+            const to = nodes.find((n) => n.id === nextId)!;
+            const x1 = from.left + 5;
+            const y1 = from.top + 5;
+            const x2 = to.left + 5;
+            const y2 = to.top + 5;
+            return (
+              <line
+                key={`${id}-${nextId}`}
+                x1={x1}
+                y1={y1}
+                x2={x2}
+                y2={y2}
+                stroke="#fff"
+                strokeWidth={2}
+              />
+            );
+          })}
+        </svg>
+        {nodes.map((node) => (
+          <MapNode
+            key={node.id}
+            top={node.top}
+            left={node.left}
+            name={node.name}
+            active={node.active}
+            admin={node.admin}
+            account={node.account}
+            onClick={() => toggleNode(node.id)}
+          />
+        ))}
+      </div>
+      <div className="button-container">
+        <button className="button">Cancel</button>
+        <button className="button">Connect</button>
+      </div>
     </div>
   );
 }
