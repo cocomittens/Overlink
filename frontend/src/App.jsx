b6c43665e07f5e7161237a02530a1261bdad444f
Coco <corriestoddard@gmail.com>
2025-06-18 17:14:08 -0700

    Add home component and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css"
 import Missions from './scenes/Missions.tsx'
 import Login from './scenes/Login.tsx'
 import Map from './scenes/Map.tsx'
+import Home from './scenes/Home.tsx'
+import Terminal from './scenes/Terminal.tsx'
 import NavBar from './components/NavBar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
@@ -11,10 +13,11 @@ function App() {
     <Router>
       <NavBar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/map" element={<Map />} />
         <Route path="/missions" element={<Missions />} />
-        <Route path="*" element={<Login />} />
+        <Route path="/terminal" element={<Terminal />} />
       </Routes>
     </Router>
   )
---
+++ frontend/src/scenes/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useAtom } from "jotai";
+import "../styles/terminal.scss";
+
+export default function Terminal() {
+    return (
+        <div className="home">
+            Welcome to the game.
+        </div>
+    );
+}
