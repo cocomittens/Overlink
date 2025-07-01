b584f110e8f9abdd39326d627442395313e42dca
Coco <corriestoddard@gmail.com>
2025-07-01 16:33:33 -0700

    Add cancel icon to trace tracker

--- frontend/src/components/TraceTracker.tsx
+++ frontend/src/components/TraceTracker.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "../styles/bottomMenu.scss";
+import CancelIcon from "./CancelIcon";
 
 const TraceTracker: React.FC = () => {
     return (
         <div className="trace-tracker">
             <span>50%</span>
+            <CancelIcon />
         </div>
     );
 };
