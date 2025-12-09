19d1a0151e5cf15cf4994e08ee72e92632900782
Coco <corriestoddard@gmail.com>
2025-12-09 14:16:13 -0800

    Change home text

--- frontend/src/scenes/Home.tsx
+++ frontend/src/scenes/Home.tsx
@@ -3,15 +3,22 @@ import "../styles/home.scss";
 
 export default function Home() {
   const [traced, setTraced] = useState(false);
+  const [showWelcome, setShowWelcome] = useState(false);
 
   useEffect(() => {
     const flag = sessionStorage.getItem("traced");
     setTraced(flag === "1");
+    const welcomeSeen = sessionStorage.getItem("welcomeSeen");
+    setShowWelcome(!welcomeSeen);
+    return () => {
+      sessionStorage.removeItem("traced");
+      sessionStorage.setItem("welcomeSeen", "1");
+    };
   }, []);
 
   return (
     <div className="home">
-      {traced ? "You have been traced." : "Welcome to the game."}
+      {traced ? "You have been traced." : showWelcome ? "Welcome to the game." : ""}
     </div>
   );
 }
